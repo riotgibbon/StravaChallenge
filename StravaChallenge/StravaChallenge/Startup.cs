824e6aaecb7d7cc6a1c134d2395e94a653a84973
Toby <tobysevans@gmail.com>
2014-07-10 21:47:22 +0100

    Added MVC wrapper

---
+++ StravaChallenge/StravaChallenge/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StravaChallenge.Startup))]
+namespace StravaChallenge
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
