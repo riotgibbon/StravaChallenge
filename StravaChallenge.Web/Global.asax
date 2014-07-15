0740eb368ed5a909a1789729d8368e4c5e70324d
Toby <tobysevans@gmail.com>
2014-07-15 12:21:05 +0100

    OAuth callback working

---
+++ StravaChallenge.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="App.StravaChallenge.Web.Application" Language="C#" %>
---
+++ StravaChallenge.Web/exchangetoken.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using IdentityManagement;
+using IdentityManagement.Strava;
+using Microsoft.Owin;
+
+namespace App.StravaChallenge.Web
+{
+    public partial class exchangetoken : System.Web.UI.Page
+    {
+        private HttpClient httpClient = new HttpClient();
+
+        protected async void Page_Load(object sender, EventArgs e)
+        {
+            string code = null;
+            var query = Request.QueryString;
+            IList<string> values = query.GetValues("code");
+            if (values != null && values.Count == 1)
+            {
+                code = values[0];
+            }
+            else
+            {
+                Response.Redirect("~/",false);
+            }
+            var details = new StravaDetails
+            {
+                ClientId = StravaConfiguration.ClientId,
+                ClientSecret = StravaConfiguration.ClientSecret,
+                Host = StravaConfiguration.StravaHost
+            };
+            var connectToStrava = new ConnectToStrava(this.httpClient, details);
+            string redirectUri="/stats";
+            var tokenResponse = await connectToStrava.SwapCodeForToken(code, redirectUri);
+            Response.Cookies.Add(new HttpCookie("token", tokenResponse.access_token)); 
+
+        }
+    }
+}
